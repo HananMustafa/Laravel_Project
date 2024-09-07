f2008828c01895d8449c5378057c6c359c9aa2b9
HananMustafa <hananmustafaawan@gmail.com>
2024-09-07 23:42:59 +0500

    design for forgot password blade

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,10 +1,14 @@
-<!-- resources/views/auth/forgot-password.blade.php -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <div class= "formbg">
     <h1>Forgot Password</h1>
 
     @if (session('status'))
@@ -15,14 +19,18 @@
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
+
+        <div class="field">
         <label for="email">Email Address</label>
         <input id="email" type="email" name="email" required autofocus>
+        </div>
         
         @error('email')
             <span class="text-red-600">{{ $message }}</span>
         @enderror
 
-        <button type="submit">Send Password Reset Link</button>
+        <button type="submit" class="btn-submit">Send Password Reset Link</button>
     </form>
+    </div>
 </body>
 </html>
